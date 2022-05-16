00cd1355e7e9facde6077ca433e5574476b83550
Даутов Самир <dautov@vbankcenter.ru>
2022-05-16 13:05:33 +0300

    Добавил readme.md

--- src/main/java/com/example/kinscopetus/KinscopeTusApplication.java
+++ src/main/java/com/example/kinscopetus/KinscopeTusApplication.java
@@ -45,63 +45,66 @@ public class KinscopeTusApplication {
 	public CommandLineRunner CommandLineRunnerBean() {
 		return (args) -> {
 
-			File file = new File("src/main/resources/zov.mp4");
+			if (args.length > 0) {
+				File file = new File("src/main/resources/zov.mp4");
 
-			UploadInfo init = init((int) file.length());
+				UploadInfo init = init((int) file.length());
 
 
-			// Create a new TusClient instance
-			TusClient client = new TusClient();
+				// Create a new TusClient instance
+				TusClient client = new TusClient();
 
-			client.setUploadCreationURL(new URL(init.getUrl()));
+				client.setUploadCreationURL(new URL(init.getUrl()));
 
-			final TusUpload upload = new TusUpload(file);
+				final TusUpload upload = new TusUpload(file);
 
 //		upload.setMetadata(Map.of("id", init.getId()));
 
-			client.enableResuming(new TusURLMemoryStore());
+				client.enableResuming(new TusURLMemoryStore());
 
-			System.out.println("Starting upload...");
+				System.out.println("Starting upload...");
 
-			TusExecutor executor = new TusExecutor() {
-				@SneakyThrows
-				@Override
-				protected void makeAttempt() throws ProtocolException, IOException {
-					// First try to resume an upload. If that's not possible we will create a new
-					// upload and get a TusUploader in return. This class is responsible for opening
-					// a connection to the remote server and doing the uploading.
-					//	TusUploader uploader = client.resumeOrCreateUpload(upload);
+				TusExecutor executor = new TusExecutor() {
+					@SneakyThrows
+					@Override
+					protected void makeAttempt() throws ProtocolException, IOException {
+						// First try to resume an upload. If that's not possible we will create a new
+						// upload and get a TusUploader in return. This class is responsible for opening
+						// a connection to the remote server and doing the uploading.
+						//	TusUploader uploader = client.resumeOrCreateUpload(upload);
 
-					// Alternatively, if your tus server does not support the Creation extension
-					// and you obtained an upload URL from another service, you can instruct
-					// tus-java-client to upload to a specific URL. Please note that this is usually
-					// _not_ necessary and only if the tus server does not support the Creation
-					// extension. The Vimeo API would be an example where this method is needed.
-					TusUploader uploader = client.beginOrResumeUploadFromURL(upload, new URL(init.getUrl()));
+						// Alternatively, if your tus server does not support the Creation extension
+						// and you obtained an upload URL from another service, you can instruct
+						// tus-java-client to upload to a specific URL. Please note that this is usually
+						// _not_ necessary and only if the tus server does not support the Creation
+						// extension. The Vimeo API would be an example where this method is needed.
+						TusUploader uploader = client.beginOrResumeUploadFromURL(upload, new URL(init.getUrl()));
 
-					// Upload the file in chunks of 1KB sizes.
-					uploader.setChunkSize(1024);
+						// Upload the file in chunks of 1KB sizes.
+						uploader.setChunkSize(1024);
 
-					// Upload the file as long as data is available. Once the
-					// file has been fully uploaded the method will return -1
-					do {
-						// Calculate the progress using the total size of the uploading file and
-						// the current offset.
-						long totalBytes = upload.getSize();
-						long bytesUploaded = uploader.getOffset();
-						double progress = (double) bytesUploaded / totalBytes * 100;
+						// Upload the file as long as data is available. Once the
+						// file has been fully uploaded the method will return -1
+						do {
+							// Calculate the progress using the total size of the uploading file and
+							// the current offset.
+							long totalBytes = upload.getSize();
+							long bytesUploaded = uploader.getOffset();
+							double progress = (double) bytesUploaded / totalBytes * 100;
 
-						System.out.printf("Upload at %06.2f%%.\n", progress);
-					} while (uploader.uploadChunk() > -1);
+							System.out.printf("Upload at %06.2f%%.\n", progress);
+						} while (uploader.uploadChunk() > -1);
 
-					// Allow the HTTP connection to be closed and cleaned up
-					uploader.finish();
+						// Allow the HTTP connection to be closed and cleaned up
+						uploader.finish();
 
-					System.out.println("Upload finished.");
-					System.out.format("Upload available at: %s", uploader.getUploadURL().toString());
-				}
-			};
-			executor.makeAttempts();
+						System.out.println("Upload finished.");
+						System.out.format("Upload available at: %s", uploader.getUploadURL().toString());
+					}
+				};
+				executor.makeAttempts();
+			}
+
 		};
 
 	}
